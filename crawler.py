f7810082ea80c12927e2888f9ad72b515de388bc
Hansel Dunlop <hansel@interpretthis.org>
2019-04-15 18:45:59 +0100

    Add crawler and tests

---
+++ crawler.py
@@ -0,0 +1,42 @@
+from html.parser import HTMLParser
+
+class LinkParser(HTMLParser):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.link_hrefs = set()
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'a':
+            for name, value in attrs:
+                if name == 'href':
+                    self.link_hrefs.add(value)
+
+
+
+def extract_links(text):
+    parser = LinkParser()
+    parser.feed(text)
+    return list(parser.link_hrefs)
+
+
+class Crawler:
+
+    def __init__(self, url, getter):
+        self.links_to_visit = [url]
+        self.visited_links = []
+        self.get = getter
+
+    def start(self, iterations):
+        links_discovered = []
+        for link in self.links_to_visit:
+            response = self.get(link)
+            links = extract_links(response.content)
+            links_discovered.extend(links)
+            self.visited_links.append(link)
+
+        self.links_to_visit = links_discovered
+
+        if iterations > 0:
+            self.start(iterations - 1)
+
---
+++ test_crawler.py
@@ -0,0 +1,44 @@
+import pytest
+import requests
+
+import crawler
+
+
+@pytest.fixture
+def getter():
+    def mock_get(url):
+
+        def internet_forever():
+            counter = 1
+            while True:
+                response = requests.Response()
+                response._content = f'<html><a href="https://www{counter}.touchsurgery.com"></a></html>'
+                counter += 1
+                yield response
+
+        if not hasattr(mock_get, 'generator'):
+            mock_get.generator = internet_forever()
+
+        return next(mock_get.generator)
+
+    return mock_get
+
+
+def test_crawler_visits_site_and_discovers_links(getter):
+    spider = crawler.Crawler('https://www.example.com', getter)
+
+    spider.start(iterations=0)
+
+    assert spider.links_to_visit == ['https://www1.touchsurgery.com']
+
+
+def test_crawler_recurses_into_discovered_links(getter):
+    spider = crawler.Crawler('https://www.example.com', getter)
+
+    spider.start(iterations=2)
+
+    assert spider.visited_links == [
+        'https://www.example.com',
+        'https://www1.touchsurgery.com',
+        'https://www2.touchsurgery.com'
+    ]
