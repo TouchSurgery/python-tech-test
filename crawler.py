f63a9ae7a8b010f5568f6a7a6553b118ec6a8efa
Hansel Dunlop <hansel@interpretthis.org>
2019-04-16 18:58:03 +0100

    Add test for relative urls

--- crawler.py
+++ crawler.py
@@ -1,6 +1,26 @@
 from html.parser import HTMLParser
 
 
+class Crawler:
+    def __init__(self, url, getter):
+        self.links_to_visit = [url]
+        self.visited_links = []
+        self.get = getter
+
+    def start(self, iterations):
+        links_discovered = []
+        for link in self.links_to_visit:
+            response = self.get(link)
+            links = extract_links(response.content, link)
+            links_discovered.extend(links)
+            self.visited_links.append(link)
+
+        self.links_to_visit = links_discovered
+
+        if iterations > 0:
+            self.start(iterations - 1)
+
+
 class LinkParser(HTMLParser):
     def __init__(self, base_url, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -20,23 +40,3 @@ def extract_links(text, base_url):
     parser = LinkParser(base_url)
     parser.feed(str(text, "utf8"))
     return list(parser.link_hrefs)
-
-
-class Crawler:
-    def __init__(self, url, getter):
-        self.links_to_visit = [url]
-        self.visited_links = []
-        self.get = getter
-
-    def start(self, iterations):
-        links_discovered = []
-        for link in self.links_to_visit:
-            response = self.get(link)
-            links = extract_links(response.content, link)
-            links_discovered.extend(links)
-            self.visited_links.append(link)
-
-        self.links_to_visit = links_discovered
-
-        if iterations > 0:
-            self.start(iterations - 1)
--- test_crawler.py
+++ test_crawler.py
@@ -5,15 +5,17 @@ import crawler
 
 
 @pytest.fixture
-def getter():
+def getter(request):
     def mock_get(url):
         def internet_forever():
             counter = 1
             while True:
+                if "relative_url" in str(request.function):
+                    host = ""
+                else:
+                    host = "https://www.touchsurgery.com"
                 response = requests.Response()
-                page = (
-                    f'<html><a href="https://www{counter}.touchsurgery.com"></a></html>'
-                )
+                page = f'<html><a href="{host}/{counter}"></a></html>'
                 response._content = bytes(page, "utf8")
                 counter += 1
                 yield response
@@ -31,7 +33,7 @@ def test_crawler_visits_site_and_discovers_links(getter):
 
     spider.start(iterations=0)
 
-    assert spider.links_to_visit == ["https://www1.touchsurgery.com"]
+    assert spider.links_to_visit == ["https://www.touchsurgery.com/1"]
 
 
 def test_crawler_recurses_into_discovered_links(getter):
@@ -41,6 +43,18 @@ def test_crawler_recurses_into_discovered_links(getter):
 
     assert spider.visited_links == [
         "https://www.example.com",
-        "https://www1.touchsurgery.com",
-        "https://www2.touchsurgery.com",
+        "https://www.touchsurgery.com/1",
+        "https://www.touchsurgery.com/2",
+    ]
+
+
+def test_crawler_handles_discovering_relative_urls(getter, request):
+    spider = crawler.Crawler("https://www.example.com", getter)
+
+    spider.start(iterations=2)
+
+    assert spider.visited_links == [
+        "https://www.example.com",
+        "https://www.example.com/1",
+        "https://www.example.com/1/2",
     ]
